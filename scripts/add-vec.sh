1aea2aae03d103dbcec910fe44e0325e310470c8
Henry Chan <henry@kinwo.net>
2023-11-18 15:32:06 +1000

    chore(script) rename

--- scripts/add-vec.sh
+++ scripts/add-vec.sh
@@ -1,10 +1,11 @@
 # To deplopy locally; update IC_NETWORK to local. To deploy to ic; update IC_NETWORK to ic.
-IC_NETWORK=${IC_NETWORK:-local}
+# IC_NETWORK=${IC_NETWORK:-local}
+IC_NETWORK=ic
 
-CONTENT_TEXT="$(cat embeddings/arcmind-content.txt)"
-CONTENT_VECTOR="$(cat embeddings/arcmind-vec.txt)"
+CONTENT_TEXT="$(cat embeddings/nmn2-content.txt)"
+CONTENT_VECTOR="$(cat embeddings/nmn2-vec.txt)"
 
-echo Add embeddings
+echo Add embeddings on $IC_NETWORK
 echo CONTENT_TEXT=$CONTENT_TEXT
 echo CONTENT_VECTOR=$CONTENT_VECTOR
 dfx canister call --network $IC_NETWORK arcmindvectordb add "(record {content = \"$CONTENT_TEXT\"; embeddings = vec $CONTENT_VECTOR})"
