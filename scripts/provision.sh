b907b54bf78c5decb1c0fb73acb069f33674b36f
Henry Chan <henry@kinwo.net>
2023-12-28 17:23:49 +1000

    ci - add OWENR_PRINCIPAL env var validation

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,9 +1,12 @@
+# Validate required env vars
+if [[ -z "${OWENR_PRINCIPAL}" ]]; then
+  echo "OWENR_PRINCIPAL is unset."
+  exit 1
+fi
+
 # To deplopy locally, update IC_NETWORK to local. To deploy to ic, update IC_NETWORK to ic.
 IC_NETWORK=${IC_NETWORK:-local}
 echo Provisioning on $IC_NETWORK
-
-# Owner is controller canister
-OWENR_PRINCIPAL=${CONTROLLER_PRINCIPAL}
 
 # Deploy vectordb canister 
 echo Deploying vectordb canister with owner $OWENR_PRINCIPAL
